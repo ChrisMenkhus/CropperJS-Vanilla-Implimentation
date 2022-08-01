7c6e21b2f22c1ac7f98186014130fdf42cb6e269
Chris Menkhus <=>
2022-08-01 17:59:29 -0600

    Added UpdateContainer

--- CropperForBBCRM.js
+++ CropperForBBCRM.js
@@ -1,27 +1,29 @@
-;(function (container, modelInstanceId) {
-  var util = BBUI.forms.Utility
-  var cropper = undefined
+(function (container, modelInstanceId) {
+  var util = BBUI.forms.Utility;
+  var cropper = undefined;
   var elements = {
     img: null,
     imgResult: null,
-  }
-  var imgSrc = ''
+  };
+  var imgSrc = '';
 
   function FormLoad() {
     // elements.img.addEventListener('load', LoadCropper, false)
     // is this necessary?
-    LoadCropper()
+    LoadCropper();
     // or is this necessary?
   }
 
   function LoadCropper() {
     // hide relaunch action if LoadCropper() is run.
-    $('#ctrl_' + modelInstanceId.replace(/-/g, '_') + '_RELAUNCH_action').hide()
+    $(
+      '#ctrl_' + modelInstanceId.replace(/-/g, '_') + '_RELAUNCH_action'
+    ).hide();
 
-    var id = 'ctrl_' + modelInstanceId.replace(/-/g, '_') + '_PICTURE_value'
-    elements.img = document.getElementById(id).getElementsByTagName('img')[0]
+    var id = 'ctrl_' + modelInstanceId.replace(/-/g, '_') + '_PICTURE_value';
+    elements.img = document.getElementById(id).getElementsByTagName('img')[0];
 
-    InstantiateCropper()
+    InstantiateCropper();
   }
 
   function InstantiateCropper() {
@@ -34,24 +36,32 @@
       aspectRatio: 220 / 330,
       zoom: function (event) {
         if (event.detail.ratio > 1) {
-          event.preventDefault()
+          event.preventDefault();
         }
       },
-    })
+    });
   }
 
   function SaveCropper() {
-    imgSrc = cropper.getCroppedCanvas().toDataURL('image/jpeg')
-    sleep(UpdateContainer, imgSrc, imgSrc)
+    imgSrc = cropper.getCroppedCanvas().toDataURL('image/jpeg');
+    sleep(UpdateContainer, imgSrc, imgSrc);
   }
 
   function timeout(ms) {
-    return new Promise((resolve) => setTimeout(resolve, ms))
+    return new Promise((resolve) => setTimeout(resolve, ms));
   }
 
   async function sleep(fn, ...args) {
-    await timeout(500)
-    return fn(...args)
+    await timeout(500);
+    return fn(...args);
+  }
+
+  function UpdateContainer(cropped, original) {
+    var dimensions = [
+      { name: 'CROPPEDPICTURE', value: cropped },
+      { name: 'ORIGINALPICTURE', value: original },
+    ];
+    container.updateMultipleFields(modelInstanceId, dimensions);
   }
 
   container.on({
@@ -59,5 +69,5 @@
     formupdated: FormLoad,
     //beforeformconfirmed: SaveCropper,
     formconfirmed: SaveCropper,
-  })
-})
+  });
+});
